04dc60f0cc66cbfe30331025aa8a2693bd76894d
Artemizzzz <108915524+Artemizzzz@users.noreply.github.com>
2022-07-26 11:12:57 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,10 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Reset_PasswordController;
 use App\Http\Controllers\DashboardController;
-use \App\Http\Controllers\LayoutController;
-use \App\Http\Controllers\Forgot_PasswordController;
-use \App\Http\Controllers\BookingController;
-use \App\Http\Controllers\PostController;
-
+use App\Http\Controllers\Forgot_PasswordController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -44,18 +43,16 @@ Route::post('/Register',[RegisterController::class,'store'])->name('register-dat
 
 Route::get('/Login',[LoginController::class,'index'])->name('login')->middleware('guest');
 Route::post('/Login',[LoginController::class,'authenticate']);
+Route::get('/Layout',[AdminController::class,'index'])->middleware('admin');
 Route::post('/Logout',[LoginController::class,'logout'])->name('logout')->middleware('auth');
-Route::get('/Logout',[LoginController::class,'logout'])->name('logout')->middleware('auth');
+Route::get('/Logout',[LoginController::class,'logout'])->name('logout')->middleware('admin');
 
 Route::get('/Forgot_Password',[Forgot_PasswordController::class,'index'])->name('forgot')->middleware('guest');
 Route::post('/Forgot_Password',[Forgot_PasswordController::class,'authenticate']);
 
-Route::get('/Reset_Password', [Reset_PasswordController::class,'index'])->name('reset')->middleware('guest');
+Route::get('/Reset_Password', [Reset_PasswordController::class,'index'])->name('reset')->middleware('auth');
 
-Route::get('/Dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware('auth');
-
-
-Route::get('/Layout',[LayoutController::class,'index'])->name('admin')->middleware('auth');
+Route::get('/Dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware('admin');
 
 Route::get('/Booking',[BookingController::class,'index'])->name('booking');
 
@@ -64,5 +61,9 @@ Route::get('/Profile',[LoginController::class,'show'])->name('profile')->middlew
 Route::get('/Posts',[PostController::class,'index'])->name('posts');
 
 Route::get('/Posts/{post}',[PostController::class,'show']);
+
+
+
+
 
 
