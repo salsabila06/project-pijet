72884bfb1fc62f80dae7c960fe52640aad9b6929
Toto Prayogo <mail@totoprayogo.com>
2022-07-28 16:37:50 +0700

    tambahkan php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude([
+        'bootstrap',
+        'public',
+        'storage',
+        'test',
+        'vendor',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$config = new PhpCsFixer\Config();
+$config->setRiskyAllowed(true);
+
+return $config->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_extra_blank_lines' => ['tokens' => ['extra']],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    ])
+    ->setUsingCache(false)
+    ->setLineEnding(PHP_EOL)
+    ->setFinder($finder);
