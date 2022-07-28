4b2924250e02c8831cd78100fbec04e00a9e7ec6
Artemizzzz <108915524+Artemizzzz@users.noreply.github.com>
2022-07-28 11:23:35 +0700

    Add files via upload

--- app/Http/Controllers/Forgot_PasswordController.php
+++ app/Http/Controllers/Forgot_PasswordController.php
@@ -2,20 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\data;
 use App\Models\reset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\MalasNgodingEmail;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class Forgot_PasswordController extends Controller
 {
-    public function index()
+    public function showForgotForm()
     {
         return view('Forgot_Password.Index');
     }
 
-    public function authenticate(Request $request)
+    public function sendResetLink(Request $request)
     {
         $request->validate([
             'email' => 'required|email|exists:data,email'
@@ -26,8 +27,13 @@ class Forgot_PasswordController extends Controller
             'token'=>$token,
             'created_at'=>Carbon::now(),
         ]);
+        $action_link=route('reset.password.form',['token'=>$token,'email'=>$request->email]);
 
-        Mail::to($request->email)->send(new MalasNgodingEmail());
+        Mail::send('TestMail.Index',['action_link'=>$action_link], function ($message) use ($request){
+          $message->from('info@belajarlaravel.com');
+          $message->to($request->email)
+              ->subject('Reset Password');
+        });
 
         return back()->with('success', 'We have e-mailed your password reset link');
 
@@ -42,9 +48,39 @@ class Forgot_PasswordController extends Controller
 */
     }
 
-    public function show(Request $request, $token=null)
+    public function showResetForm(Request $request, $token=null  )
     {
         return view('Reset_Password.Index')->with(['token'=>$token,'email'=>$request->email]);
     }
-    //
+
+    public function resetPassword(Request $request)
+    {
+
+        $request->validate([
+            'email'=>'required|email|exists:data,email',
+            'password'=>'required|min:5|confirmed',
+            'password_confirmation'=>'required',
+        ]);
+
+        $check_token=reset::where([
+            'email'=>$request->email,
+            'token'=>$request->token
+        ])->first();
+
+
+        if (!$check_token){
+            return back()->withInput()->with('fail','Invalid Token');
+        }else{
+
+            data::where('email',$request->email)->update([
+                'password'=>Hash::make($request->password)
+            ]);
+
+            reset::where([
+                'email'=>$request->email
+            ])->delete();
+
+            return redirect()->route('login')->with('success','Your password has been change! You can login with the new password')->with('verifiedEmail',$request->email);
+        }
+    }
 }
