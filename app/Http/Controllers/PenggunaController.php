86e78c879146c9ae1e09ed330cada724b4d70eec
Agung026 <109585172+Agung026@users.noreply.github.com>
2022-07-29 11:45:15 +0700

    Add files via upload

---
+++ app/Http/Controllers/PenggunaController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pengguna;
+use Illuminate\Http\Request;
+
+class PenggunaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pengguna = pengguna::all();
+        return view('Pengguna.index',compact('pengguna'));
+    }
+
+    public function detail_pengguna(pengguna $data)
+    {
+        return view('Detail_Pengguna.Index',['data'=>$data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\pengguna  $pengguna
+     * @return \Illuminate\Http\Response
+     */
+    public function show(pengguna $pengguna)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\pengguna  $pengguna
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(pengguna $pengguna)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\pengguna  $pengguna
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, pengguna $pengguna)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\pengguna  $pengguna
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(pengguna $pengguna)
+    {
+        //
+    }
+}
