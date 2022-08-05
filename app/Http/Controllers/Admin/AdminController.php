9dc1e6918d29cf4cd5549e1c55d3ec965b7733ac
Artemizzzz <108915524+Artemizzzz@users.noreply.github.com>
2022-08-05 13:54:50 +0700

    Add files via upload

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -41,7 +41,7 @@ class AdminController extends Controller
      */
     public function authenticate(LoginAdminRequest $request)
     {
-        $request=authenticate();
+        $request->authenticate();
 
         $request->session()->regenerate();
 
