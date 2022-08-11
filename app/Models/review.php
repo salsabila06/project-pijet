8ec9b02b407e7055cc951723ddd20f8fcab9487a
Agung026 <109585172+Agung026@users.noreply.github.com>
2022-08-11 09:15:48 +0700

    Add files via upload

---
+++ app/Models/review.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class review extends Model
+{
+    use HasFactory;
+
+    protected $table='review';
+    protected $guarded=['id'];
+}
