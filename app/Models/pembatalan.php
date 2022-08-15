28ad9407e94325193206ee67a7b4b6553cfdfd9c
Agung026 <109585172+Agung026@users.noreply.github.com>
2022-08-15 14:51:31 +0700

    Add files via upload

---
+++ app/Models/pembatalan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pembatalan extends Model
+{
+    use HasFactory;
+
+    protected $table='pembatalan';
+    protected $guarded='id';
+}
