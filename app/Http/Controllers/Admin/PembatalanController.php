101585e11b9250930cd79b7ffa6c5c8166197113
Agung026 <109585172+Agung026@users.noreply.github.com>
2022-08-15 14:50:35 +0700

    Update PembatalanController.php

--- app/Http/Controllers/Admin/PembatalanController.php
+++ app/Http/Controllers/Admin/PembatalanController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\pembatalan;
 
 class PembatalanController extends Controller
 {
 
     public function index()
     {
-        return view('Admin.Pembatalan.index');
+        $pembatalan = pembatalan::paginate(10);
+        return view('Admin.Pembatalan.index',compact('pembatalan'));
     }
-    //
+
+    public function detail()
+    {
+        return view('Admin.Pembatalan.detail');
+    }
 }
