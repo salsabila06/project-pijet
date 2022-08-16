7b0200f76a7850758d02086f8d3be72570b9c353
Artemizzzz <108915524+Artemizzzz@users.noreply.github.com>
2022-08-16 17:09:46 +0700

    Update data.php

--- app/Models/data.php
+++ app/Models/data.php
@@ -14,6 +14,14 @@ class data extends Model
     protected $fillable=['username','first_name','last_name','email','password'];
     protected $guarded='id';
 
+    public function scopeFilters($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query,$search){
+            return $query->where('username', 'like', "%{$search}%")
+                ->orwhere('email', 'like', "%{$search}%");
+        });
+    }
+    
     public function verifikasi()
     {
         return $this->belongsTo(verifikasi::class);
