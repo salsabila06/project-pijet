f443dd14f8896653b6cd8eea5c198260f4e3a436
Artemizzzz <108915524+Artemizzzz@users.noreply.github.com>
2022-08-16 17:10:14 +0700

    Update pembatalan.php

--- app/Models/pembatalan.php
+++ app/Models/pembatalan.php
@@ -11,4 +11,12 @@ class pembatalan extends Model
 
     protected $table='pembatalan';
     protected $guarded='id';
+    
+    public function scopeFilters($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query,$search){
+            return $query->where('username', 'like', "%{$search}%")
+                ->orwhere('email', 'like', "%{$search}%");
+        });
+    }
 }
