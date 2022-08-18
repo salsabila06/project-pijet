cfac40ced895a6a30fff371d1fdba9009f1ec050
Agung026 <109585172+Agung026@users.noreply.github.com>
2022-08-18 20:07:42 +0700

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\data;
+use App\Models\pemesanan;
 
 class DashboardController extends Controller
 {
 
     public function index()
     {
-        return view('Admin.Dashboard.Index');
+        return view('Admin.Dashboard.Index',[
+            'jumlah_data' => data::all()->count(), 'jumlah_booking' => pemesanan::all()->count(),
+        ]);
     }
-    //
 }
