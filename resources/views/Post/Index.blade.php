e9c60277aeefbdec7266375d1d631dac13d984af
Agung026 <109585172+Agung026@users.noreply.github.com>
2022-08-25 12:21:49 +0700

    Update Index.blade.php

--- resources/views/Post/Index.blade.php
+++ resources/views/Post/Index.blade.php
@@ -1,7 +1,26 @@
+<html>
 
-<h1 class="mb-5">{{$post->title}}</h1>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- @TODO: replace SET_YOUR_CLIENT_KEY_HERE with your client key -->
+    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
+        data-client-key="SB-Mid-client-HeXtdjE4jt9eAhwn"></script>
+    <!-- Note: replace with src="https://app.midtrans.com/snap/snap.js" for Production environment -->
+</head>
 
-{{$post->body}}
+<body>
+    <button id="pay-button">Pay!</button>
 
-<a href="{{route('posts')}}>Back to Posts</a>
+    <script type="text/javascript">
+        // For example trigger on button clicked, or any time you need
+        var payButton = document.getElementById('pay-button');
+        payButton.addEventListener('click', function() {
+            // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
+            window.snap.pay('{{ $snap_token }}');
 
+            // customer will be redirected after completing payment pop-up
+        });
+    </script>
+</body>
+
+</html>
