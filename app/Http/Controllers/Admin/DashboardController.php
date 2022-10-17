ed2b22b4e330cd45223abe0e890de64374a25776
Agung026 <109585172+Agung026@users.noreply.github.com>
2022-10-17 08:49:46 +0700

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\data;
 use App\Models\pemesanan;
+use App\Models\pengguna;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
 
     public function index()
-    {
-        return view('Admin.Dashboard.Index',[
-            'jumlah_data' => data::all()->count(), 'jumlah_booking' => pemesanan::all()->count(),
-        ]);
+     {
+        $jumlah_data = pengguna::all()->count();
+        $jumlah_booking = pemesanan::all()->count();
+        $profit = DB::table('jenispijat')
+                            ->join('pemesanan', function($join){
+                                $join->on('jenispijat.id','=','pemesanan.jenispijat_id');
+                            })
+                            ->sum('harga');
+        return view('Admin.Dashboard.Index',compact('jumlah_data','jumlah_booking','profit'));
     }
 }
+
