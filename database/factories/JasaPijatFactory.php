f6b6e75f74204a40bb10d2fb11c1e21db2cb5f66
RAJA12 <76493200+Raja-bot@users.noreply.github.com>
2022-11-04 10:24:28 +0700

    Update JasaPijatFactory.php

--- database/factories/JasaPijatFactory.php
+++ database/factories/JasaPijatFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\jasaPijat>
  */
-class JasaPijatFactory extends Factory
+class jasaPijatFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,6 @@ class JasaPijatFactory extends Factory
     public function definition()
     {
         return [
-            'pemijat'=>$this->faker->userName(),
             'jasa_pijat'=>$this->faker->jobTitle(),
             'harga'=>$this->faker->randomDigitNotNull(),
         ];
